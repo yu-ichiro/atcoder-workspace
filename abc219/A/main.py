098bf1b983f34f3c164fcf398ccd9d5c4aab8369
Yuichiro Smith <contact@yu-smith.com>
2021-09-18 21:02:24 +0900

    2021/09/18 ABC219 A AC

--- abc219/A/main.py
+++ abc219/A/main.py
@@ -5,7 +5,12 @@ import sys
 
 
 def solve(X: int):
-    return
+    print(
+        40 - X if X < 40 else
+        70 - X if X < 70 else
+        90 - X if X < 90 else
+        "expert"
+    )
 
 
 def main():
